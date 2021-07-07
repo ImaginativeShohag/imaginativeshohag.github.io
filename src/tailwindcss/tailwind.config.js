9030fc9d6b6ef01dd5591db26ed149f3a37d52ba
Md. Mahmudul Hasan Shohag <imaginativeshohag@gmail.com>
2021-07-07 11:35:09 +0600

    Post, tag, category page design tweaked

--- src/tailwindcss/tailwind.config.js
+++ src/tailwindcss/tailwind.config.js
@@ -5,14 +5,15 @@ module.exports = {
         './_includes/**/*.html',
         './_layouts/**/*.html',
         './_posts/*.md',
+        './_draft/*.md',
         './*.html',
     ],
     darkMode: 'class', // or 'media' or 'class'
     theme: {
         fontFamily: {
-            sans: ['Nunito', 'sans-serif'],
-            serif: ['Arvo', 'serif'],
-            mono: ['JetBrains Mono', 'monospace'],
+            sans: ['Nunito', 'ui-sans-serif', 'system-ui', '-apple-system', 'BlinkMacSystemFont', "Segoe UI", 'Roboto', "Helvetica Neue", 'Arial', "Noto Sans", 'sans-serif', "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji"],
+            serif: ['Arvo', 'ui-serif', 'Georgia', 'Cambria', "Times New Roman", 'Times', 'serif'],
+            mono: ['JetBrains Mono', 'ui-monospace', 'SFMono-Regular', 'Menlo', 'Monaco', 'Consolas', "Liberation Mono", "Courier New", 'monospace'],
         },
         colors: {
             transparent: 'transparent',
